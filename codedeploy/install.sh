38afb2167f5504a58915c282cd3bc88c60335228
thaleslnr <44848784+thaleslnr@users.noreply.github.com>
2018-11-26 13:50:15 -0200

    Update install.sh

--- codedeploy/install.sh
+++ codedeploy/install.sh
@@ -1,3 +1,3 @@
-cp /var/www/html/urt/codedeploy/zz_urt.conf /etc/httpd/conf.d/
-service httpd restart
+cp /var/www/html/urt/codedeploy/zz_urt.conf /etc/nginx/conf.d/
+service nginx restart
 chmod -R 777 /var/www
