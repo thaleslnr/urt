fa006b25df8ccd65f0a924f60939db6512010d8a
thaleslnr <44848784+thaleslnr@users.noreply.github.com>
2018-11-26 14:56:45 -0200

    Update install.sh

--- codedeploy/install.sh
+++ codedeploy/install.sh
@@ -1,3 +1,4 @@
-cp /var/www/html/urt/codedeploy/zz_urt.conf /etc/nginx/conf.d/
+cp /var/www/html/urt/codedeploy/zz_urt.conf /etc/nginx/sites-available/
+ls -s /etc/nginx/sites-available/ /etc/nginx/sites-enabled/
 service nginx restart
 chmod -R 777 /var/www
